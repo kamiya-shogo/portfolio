616d1aab37b8b85789c6c73348a6decac5aacfb7
kamiya-shogo <kamiya_shogo@icloud.com>
2022-11-22 19:58:25 +0900

    C1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20221117104209) do
+ActiveRecord::Schema.define(version: 20221122105347) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
